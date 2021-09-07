0907d755628b979721849b55bd3c338cdb03bce9
MelissaQ1999 <61278416+MelissaQ1999@users.noreply.github.com>
2021-09-07 16:01:14 +0200

    all new

--- script/main.js
+++ script/main.js
@@ -1,5 +1,23 @@
 // ----- SITE LOADER ----- //
-$(window).on("load",function(){
-    $("#loaderInner").fadeOut("slow");
-    $("#doc-loader").delay(400).fadeOut("slow");
+$(window).on('load',function(){
+    $('#loaderInner').fadeOut('slow');
+    $('#doc-loader').delay(400).fadeOut('slow');
+});
+
+// ----- Hamburgermenu (the three bars) ----- //
+$(document).ready(function(){
+	// Menu click event
+	$('.menuBtn').click(function() {
+		$(this).toggleClass('active');
+			if($(this).hasClass('active')) {
+                $('html').css('overflow', 'hidden');
+                $('.mainMenu').addClass('act');
+                $('header').addClass('act');
+            }
+			else {
+                $('html').css('overflow', 'auto');
+                $('.mainMenu').removeClass('act');
+                $('header').removeClass('act');
+            }
+    });
 });
