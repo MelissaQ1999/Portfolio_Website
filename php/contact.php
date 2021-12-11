16072be6ba9f6de287850a6d7926a065f5348e89
MelissaQ1999 <61278416+MelissaQ1999@users.noreply.github.com>
2021-12-11 20:58:57 +0100

    minor updates

--- php/contact.php
+++ php/contact.php
@@ -1,9 +1,9 @@
 <?php
 
-$name = $email = $topic = $message = "";
+$name = $emailuser = $topic = $message = "";
 
 $name = test_input($_POST['name']);
-$email = $_POST['email'];
+$emailuser = $_POST['email'];
 $topic = $_POST['topic'];
 $message = $_POST['message'];
 
@@ -14,7 +14,28 @@ function test_input($data) {
     return $data;
 }
 
-echo($name);
+
+// sendgrid
+require '../sendgrid-php/vendor/autoload.php';
+
+$email = new \SendGrid\Mail\Mail(); 
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("test@example.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
+
 
 // header('Location: ../pages/contact.html');
 
